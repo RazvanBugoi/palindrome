46f27d9703248919a43490c8344b9e7dd340d418
RazvanBugoi <razvanbugoi@gmail.com>
2020-10-10 20:17:36 +0100

    added onclick event

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,14 @@
+let btn = document.getElementById('add');
+let inputValue = document.getElementById('input');
+
 function palindrome(str) {
     return str.split('').reverse().join('') === str;
+}
+
+btn.onclick = () => {
+    if (palindrome(inputValue.value)) {
+        alert('Succes! The input word is a palindrome.');
+    } else {
+        alert('Not this time! This word is not a palindrome');
+    }
 }
